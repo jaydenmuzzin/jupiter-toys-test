d943ff724aebab0dc64a2af6f73636c278afc0ea
Jayden Muzzin <jayden.muzzin@outlook.com>
2025-05-21 14:37:35 +0800

    test(purchasing): Add Test Case 3
    Test Case 3 verifies that the cart displays the correct prices and
    calculations upon navigating to the Cart page after opting to buy
    multiple products from the Shop page.

---
+++ tests/purchasing.spec.ts
@@ -0,0 +1,183 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Purchasing", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/#/shop");
+    });
+
+    test("should have correct prices and calculations in cart", async ({
+        page,
+    }) => {
+        const STUFFED_FROG_PROD = page
+            .getByRole("listitem")
+            .filter({ hasText: "Stuffed Frog" });
+
+        const STUFFED_FROG_PROD_PRICE = (
+            await STUFFED_FROG_PROD.locator(".product-price").textContent()
+        )?.replace("$", "");
+
+        await STUFFED_FROG_PROD.getByRole("link", { name: "Buy" }).click();
+        await STUFFED_FROG_PROD.getByRole("link", { name: "Buy" }).click();
+
+        const FLUFFY_BUNNY_PROD = page
+            .getByRole("listitem")
+            .filter({ hasText: "Fluffy Bunny" });
+        const FLUFFY_BUNNY_PROD_PRICE = (
+            await FLUFFY_BUNNY_PROD.locator(".product-price").textContent()
+        )?.replace("$", "");
+
+        await FLUFFY_BUNNY_PROD.getByRole("link", { name: "Buy" }).click();
+        await FLUFFY_BUNNY_PROD.getByRole("link", { name: "Buy" }).click();
+        await FLUFFY_BUNNY_PROD.getByRole("link", { name: "Buy" }).click();
+        await FLUFFY_BUNNY_PROD.getByRole("link", { name: "Buy" }).click();
+        await FLUFFY_BUNNY_PROD.getByRole("link", { name: "Buy" }).click();
+
+        const VALENTINE_BEAR_PROD = page
+            .getByRole("listitem")
+            .filter({ hasText: "Valentine Bear" });
+        const VALENTINE_BEAR_PROD_PRICE = (
+            await VALENTINE_BEAR_PROD.locator(".product-price").textContent()
+        )?.replace("$", "");
+
+        await VALENTINE_BEAR_PROD.getByRole("link", {
+            name: "Buy",
+        }).click();
+        await VALENTINE_BEAR_PROD.getByRole("link", {
+            name: "Buy",
+        }).click();
+        await VALENTINE_BEAR_PROD.getByRole("link", {
+            name: "Buy",
+        }).click();
+
+        await test.step("Navigate to 'Cart' page", async () => {
+            await page.getByRole("link", { name: "Cart" }).click();
+            await page.waitForURL("**/cart");
+            await expect(
+                page.getByRole("listitem").filter({ hasText: "Cart" })
+            ).toHaveClass("active");
+        });
+
+        const STUFFED_FROG_PROD_IN_CART = page.getByRole("row").filter({
+            has: page.getByRole("cell").filter({ hasText: "Stuffed Frog" }),
+        });
+
+        const STUFFED_FROG_PROD_CART_PRICE =
+            (
+                await STUFFED_FROG_PROD_IN_CART.evaluate(
+                    (tr) => tr.children[1].textContent
+                )
+            )?.replace("$", "") || "0";
+
+        const STUFFED_FROG_PROD_CART_QUANTITY =
+            await STUFFED_FROG_PROD_IN_CART.getByRole(
+                "spinbutton"
+            ).inputValue();
+
+        const STUFFED_FROG_PROD_CART_SUBTOTAL =
+            (
+                await STUFFED_FROG_PROD_IN_CART.evaluate(
+                    (tr) => tr.children[3].textContent
+                )
+            )?.replace("$", "") || "0";
+
+        const FLUFFY_BUNNY_PROD_IN_CART = page.getByRole("row").filter({
+            has: page.getByRole("cell").filter({ hasText: "Fluffy Bunny" }),
+        });
+
+        const FLUFFY_BUNNY_PROD_CART_PRICE =
+            (
+                await FLUFFY_BUNNY_PROD_IN_CART.evaluate(
+                    (tr) => tr.children[1].textContent
+                )
+            )?.replace("$", "") || "0";
+
+        const FLUFFY_BUNNY_PROD_CART_QUANTITY =
+            await FLUFFY_BUNNY_PROD_IN_CART.getByRole(
+                "spinbutton"
+            ).inputValue();
+
+        const FLUFFY_BUNNY_PROD_CART_SUBTOTAL =
+            (
+                await FLUFFY_BUNNY_PROD_IN_CART.evaluate(
+                    (tr) => tr.children[3].textContent
+                )
+            )?.replace("$", "") || "0";
+
+        const VALENTINE_BEAR_PROD_IN_CART = page.getByRole("row").filter({
+            has: page.getByRole("cell").filter({ hasText: "Valentine Bear" }),
+        });
+
+        const VALENTINE_BEAR_PROD_CART_PRICE =
+            (
+                await VALENTINE_BEAR_PROD_IN_CART.evaluate(
+                    (tr) => tr.children[1].textContent
+                )
+            )?.replace("$", "") || "0";
+
+        const VALENTINE_BEAR_PROD_CART_QUANTITY =
+            await VALENTINE_BEAR_PROD_IN_CART.getByRole(
+                "spinbutton"
+            ).inputValue();
+
+        const VALENTINE_BEAR_PROD_CART_SUBTOTAL =
+            (
+                await VALENTINE_BEAR_PROD_IN_CART.evaluate(
+                    (tr) => tr.children[3].textContent
+                )
+            )?.replace("$", "") || "0";
+
+        await test.step("The subtotal for each product is correct", async () => {
+            expect.soft(
+                parseFloat(STUFFED_FROG_PROD_CART_SUBTOTAL),
+                "The Stuffed Frog's subtotal equals the product of its price and quantity"
+            ).toEqual(
+                parseFloat(STUFFED_FROG_PROD_CART_PRICE) *
+                    parseFloat(STUFFED_FROG_PROD_CART_QUANTITY)
+            );
+
+            expect(
+                parseFloat(FLUFFY_BUNNY_PROD_CART_SUBTOTAL),
+                "The Fluffy Bunny's subtotal equals the product of its price and quantity"
+            ).toEqual(
+                parseFloat(FLUFFY_BUNNY_PROD_CART_PRICE) *
+                    parseFloat(FLUFFY_BUNNY_PROD_CART_QUANTITY)
+            );
+
+            expect(
+                parseFloat(VALENTINE_BEAR_PROD_CART_SUBTOTAL),
+                "The Valentine Bear's subtotal equals the product of its price and quantity"
+            ).toEqual(
+                parseFloat(VALENTINE_BEAR_PROD_CART_PRICE) *
+                    parseFloat(VALENTINE_BEAR_PROD_CART_QUANTITY)
+            );
+        });
+
+        await test.step("The price for each product is correct", async () => {
+            expect(
+                STUFFED_FROG_PROD_CART_PRICE,
+                "The price of the Stuffed Frog in the cart equals that on the 'Shop' page"
+            ).toEqual(STUFFED_FROG_PROD_PRICE);
+
+            expect(
+                FLUFFY_BUNNY_PROD_CART_PRICE,
+                "The price of the Fluffy Bunny in the cart equals that on the 'Shop' page"
+            ).toEqual(FLUFFY_BUNNY_PROD_PRICE);
+
+            expect(
+                VALENTINE_BEAR_PROD_CART_PRICE,
+                "The price of the Valentine Bear in the cart equals that on the 'Shop' page"
+            ).toEqual(VALENTINE_BEAR_PROD_PRICE);
+        });
+
+        await expect(
+            page.locator(".total"),
+            "The total equals the sum of all subtotals"
+        ).toContainText(
+            (
+                parseFloat(STUFFED_FROG_PROD_CART_SUBTOTAL) +
+                parseFloat(FLUFFY_BUNNY_PROD_CART_SUBTOTAL) +
+                parseFloat(VALENTINE_BEAR_PROD_CART_SUBTOTAL)
+            ).toString()
+        );
+    });
+});
