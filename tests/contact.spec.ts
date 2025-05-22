328064e030594c7b08b76533945736933905cd2c
Jayden Muzzin <jayden.muzzin@outlook.com>
2025-05-22 11:55:12 +0800

    refactor(tests): Add tags
    Improved report filtering and test detailing by adding tags to tests.

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -3,114 +3,137 @@ import CONTACT_FORM_DATA from "../data/contact-form.json";
 import { ContactPage } from "../page-objects/contact-page.pom";
 import { navigateTo } from "../utils/common";
 
-test.describe("Contact page", () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto("/");
-    });
-
-    test.describe("Form behaviour", () => {
-        test("Test Case 1: should handle mandatory field errors", async ({ page }) => {
-            const CONTACT_PAGE = new ContactPage(page);
-
-            await test.step("Navigate to 'Contact' page", async () => {
-                navigateTo(page, CONTACT_PAGE);
-            });
-
-            await test.step("Header message updated to indicate errors upon submitting an invalid form", async () => {
-                await CONTACT_PAGE.submitForm();
-                await expect(CONTACT_PAGE.formInfo).toBeHidden();
-                await expect(CONTACT_PAGE.formInfoError).toBeVisible();
-            });
-
-            await test.step("Error message for each unpopulated mandatory field displays upon submitting form", async () => {
-                await expect(
-                    CONTACT_PAGE.forenameRequired,
-                    "'Forename is required' is present"
-                ).toBeVisible();
-                await expect(
-                    CONTACT_PAGE.emailRequired,
-                    "'Email is required' is present"
-                ).toBeVisible();
-                await expect(
-                    CONTACT_PAGE.messageRequired,
-                    "'Message is required' is present"
-                ).toBeVisible();
-            });
-
-            await test.step("Correctly addressing error of a mandatory field removes its message", async () => {
-                CONTACT_PAGE.enterForename("Jane");
-                await expect(
-                    CONTACT_PAGE.forenameRequired,
-                    "'Forename is required' is absent after populating field with valid data"
-                ).toBeHidden();
-
-                CONTACT_PAGE.enterEmail("jane.doe@example.com");
-                await expect(
-                    CONTACT_PAGE.emailRequired,
-                    "'Email is required' is absent after populating field with valid data"
-                ).toBeHidden();
-
-                CONTACT_PAGE.enterMessage("A toy I ordered has not arrived");
-                await expect(
-                    CONTACT_PAGE.messageRequired,
-                    "'Message is required' is absent after populating field with valid data"
-                ).toBeHidden();
-            });
-
-            await test.step("Header message returns to normal upon all errors having been correctly addressed", async () => {
-                await expect(CONTACT_PAGE.formInfo).toBeVisible();
-                await expect(CONTACT_PAGE.formInfoError).toBeHidden();
-            });
+test.describe(
+    "Contact page",
+    {
+        tag: "@contact",
+    },
+    () => {
+        test.beforeEach(async ({ page }) => {
+            await page.goto("/");
         });
 
-        // Fulfilling Test Case condition: "Note: Run this test 5 times to ensure 100% pass rate"
-        const REPEAT_TEST = 5;
+        test.describe(
+            "Form behaviour",
+            {
+                tag: "@form",
+            },
+            () => {
+                test(
+                    "Test Case 1: should handle mandatory field errors",
+                    {
+                        tag: "@validation",
+                    },
+                    async ({ page }) => {
+                    const CONTACT_PAGE = new ContactPage(page);
 
-        for (let i = 0; i < REPEAT_TEST; i++) {
-            let cfd = CONTACT_FORM_DATA[i];
-
-            test(`Test Case 2: should successfully submit having populated mandatory fields with valid data: attempt #${
-                i + 1
-            }`, async ({ page }) => {
-                const CONTACT_PAGE = new ContactPage(page);
-
-                await test.step("Navigate to 'Contact' page", async () => {
-                    navigateTo(page, CONTACT_PAGE);
-                });
-
-                await test.step(`${cfd.forename} ${cfd.surname}'s contact form submits without error`, async () => {
-                    await test.step("Form can be populated", async () => {
-                        await CONTACT_PAGE.enterForename(cfd.forename);
-                        await CONTACT_PAGE.enterSurname(cfd.surname);
-                        await CONTACT_PAGE.enterEmail(cfd.email);
-                        await CONTACT_PAGE.enterTelephone(cfd.telephone);
-                        await CONTACT_PAGE.enterMessage(cfd.message);
+                    await test.step("Navigate to 'Contact' page", async () => {
+                        navigateTo(page, CONTACT_PAGE);
                     });
 
-                    await test.step("Form can be submitted", async () => {
+                    await test.step("Header message updated to indicate errors upon submitting an invalid form", async () => {
                         await CONTACT_PAGE.submitForm();
+                        await expect(CONTACT_PAGE.formInfo).toBeHidden();
+                        await expect(CONTACT_PAGE.formInfoError).toBeVisible();
                     });
 
-                    await expect(
-                        await CONTACT_PAGE.sendingFeedbackHeading(),
-                        "'Sending Feedback' dialog is displayed while processing request"
-                    ).toBeVisible();
+                    await test.step("Error message for each unpopulated mandatory field displays upon submitting form", async () => {
+                        await expect(
+                            CONTACT_PAGE.forenameRequired,
+                            "'Forename is required' is present"
+                        ).toBeVisible();
+                        await expect(
+                            CONTACT_PAGE.emailRequired,
+                            "'Email is required' is present"
+                        ).toBeVisible();
+                        await expect(
+                            CONTACT_PAGE.messageRequired,
+                            "'Message is required' is present"
+                        ).toBeVisible();
+                    });
 
-                    // Time to complete form submission request varies, after which the form is no longer displayed.
-                    // Requests consistently complete within 20 seconds, which is deemed acceptable
-                    await expect(
-                        await CONTACT_PAGE.form(),
-                        "Contact form is absent upon completion of form submission"
-                    ).toBeHidden({
-                        timeout: 20000,
+                    await test.step("Correctly addressing error of a mandatory field removes its message", async () => {
+                        CONTACT_PAGE.enterForename("Jane");
+                        await expect(
+                            CONTACT_PAGE.forenameRequired,
+                            "'Forename is required' is absent after populating field with valid data"
+                        ).toBeHidden();
+
+                        CONTACT_PAGE.enterEmail("jane.doe@example.com");
+                        await expect(
+                            CONTACT_PAGE.emailRequired,
+                            "'Email is required' is absent after populating field with valid data"
+                        ).toBeHidden();
+
+                        CONTACT_PAGE.enterMessage("A toy I ordered has not arrived");
+                        await expect(
+                            CONTACT_PAGE.messageRequired,
+                            "'Message is required' is absent after populating field with valid data"
+                        ).toBeHidden();
+                    });
+
+                    await test.step("Header message returns to normal upon all errors having been correctly addressed", async () => {
+                        await expect(CONTACT_PAGE.formInfo).toBeVisible();
+                        await expect(CONTACT_PAGE.formInfoError).toBeHidden();
                     });
                 });
 
-                await expect(
-                    await CONTACT_PAGE.formInfoSuccessMessage(cfd.forename),
-                    "Successful submission message is present upon completion of form submission"
-                ).toBeVisible();
-            });
-        }
-    });
-});
+                // Fulfilling Test Case condition: "Note: Run this test 5 times to ensure 100% pass rate"
+                const REPEAT_TEST = 5;
+
+                for (let i = 0; i < REPEAT_TEST; i++) {
+                    let cfd = CONTACT_FORM_DATA[i];
+
+                    test(
+                        `Test Case 2: should successfully submit having populated mandatory fields with valid data: attempt #${
+                            i + 1
+                        }`,
+                        {
+                            tag: "@e2e",
+                        },
+                        async ({ page }) => {
+                            const CONTACT_PAGE = new ContactPage(page);
+
+                            await test.step("Navigate to 'Contact' page", async () => {
+                                navigateTo(page, CONTACT_PAGE);
+                            });
+
+                            await test.step(`${cfd.forename} ${cfd.surname}'s contact form submits without error`, async () => {
+                                await test.step("Form can be populated", async () => {
+                                    await CONTACT_PAGE.enterForename(cfd.forename);
+                                    await CONTACT_PAGE.enterSurname(cfd.surname);
+                                    await CONTACT_PAGE.enterEmail(cfd.email);
+                                    await CONTACT_PAGE.enterTelephone(cfd.telephone);
+                                    await CONTACT_PAGE.enterMessage(cfd.message);
+                                });
+
+                                await test.step("Form can be submitted", async () => {
+                                    await CONTACT_PAGE.submitForm();
+                                });
+
+                                await expect(
+                                    await CONTACT_PAGE.sendingFeedbackHeading(),
+                                    "'Sending Feedback' dialog is displayed while processing request"
+                                ).toBeVisible();
+
+                                // Time to complete form submission request varies, after which the form is no longer displayed.
+                                // Requests consistently complete within 20 seconds, which is deemed acceptable
+                                await expect(
+                                    await CONTACT_PAGE.form(),
+                                    "Contact form is absent upon completion of form submission"
+                                ).toBeHidden({
+                                    timeout: 20000,
+                                });
+                            });
+
+                            await expect(
+                                await CONTACT_PAGE.formInfoSuccessMessage(cfd.forename),
+                                "Successful submission message is present upon completion of form submission"
+                            ).toBeVisible();
+                        }
+                    );
+                }
+            }
+        );
+    }
+);
--- tests/purchasing.spec.ts
+++ tests/purchasing.spec.ts
@@ -3,110 +3,120 @@ import { ShopPage } from "../page-objects/shop-page.pom";
 import { CartPage } from "../page-objects/cart-page.pom";
 import { navigateTo } from "../utils/common";
 
-test.describe("Purchasing", () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto("/#/shop");
-    });
-
-    test("Test Case 3: should have correct prices and calculations in cart", async ({
-        page,
-    }) => {
-        const SHOP_PAGE = new ShopPage(page);
-        const CART_PAGE = new CartPage(page);
-
-        const STUFFED_FROG_PROD = await SHOP_PAGE.product("Stuffed Frog");
-        const STUFFED_FROG_PROD_PRICE = await SHOP_PAGE.productDecimalPrice(STUFFED_FROG_PROD);
-
-        await SHOP_PAGE.buy(STUFFED_FROG_PROD);
-        await SHOP_PAGE.buy(STUFFED_FROG_PROD);
-
-        const FLUFFY_BUNNY_PROD = await SHOP_PAGE.product("Fluffy Bunny");
-        const FLUFFY_BUNNY_PROD_PRICE = await SHOP_PAGE.productDecimalPrice(FLUFFY_BUNNY_PROD);
-
-        await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
-        await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
-        await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
-        await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
-        await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
-
-        const VALENTINE_BEAR_PROD = await SHOP_PAGE.product("Valentine Bear");
-        const VALENTINE_BEAR_PROD_PRICE = await SHOP_PAGE.productDecimalPrice(VALENTINE_BEAR_PROD);
-
-        await SHOP_PAGE.buy(VALENTINE_BEAR_PROD);
-        await SHOP_PAGE.buy(VALENTINE_BEAR_PROD);
-        await SHOP_PAGE.buy(VALENTINE_BEAR_PROD);
-
-        await test.step("Navigate to 'Cart' page", async () => {
-            navigateTo(page, CART_PAGE);
+test.describe(
+    "Purchasing",
+    {
+        tag: "@shop",
+    },
+    () => {
+        test.beforeEach(async ({ page }) => {
+            await page.goto("/#/shop");
         });
 
-        const STUFFED_FROG_PROD_IN_CART = await CART_PAGE.product("Stuffed Frog");
-        const STUFFED_FROG_PROD_CART_PRICE = await CART_PAGE.productDecimalPrice(STUFFED_FROG_PROD_IN_CART);
-        const STUFFED_FROG_PROD_CART_QUANTITY = await CART_PAGE.productQuantity(STUFFED_FROG_PROD_IN_CART);
-        const STUFFED_FROG_PROD_CART_SUBTOTAL = await CART_PAGE.productDecimalSubtotal(STUFFED_FROG_PROD_IN_CART);
+        test(
+            "Test Case 3: should have correct prices and calculations in cart",
+            {
+                tag: "@cart",
+            }, 
+            async ({ page }) => {
+                const SHOP_PAGE = new ShopPage(page);
+                const CART_PAGE = new CartPage(page);
 
-        const FLUFFY_BUNNY_PROD_IN_CART = await CART_PAGE.product("Fluffy Bunny");
-        const FLUFFY_BUNNY_PROD_CART_PRICE = await CART_PAGE.productDecimalPrice(FLUFFY_BUNNY_PROD_IN_CART);
-        const FLUFFY_BUNNY_PROD_CART_QUANTITY = await CART_PAGE.productQuantity(FLUFFY_BUNNY_PROD_IN_CART);
-        const FLUFFY_BUNNY_PROD_CART_SUBTOTAL = await CART_PAGE.productDecimalSubtotal(FLUFFY_BUNNY_PROD_IN_CART);
+                const STUFFED_FROG_PROD = await SHOP_PAGE.product("Stuffed Frog");
+                const STUFFED_FROG_PROD_PRICE = await SHOP_PAGE.productDecimalPrice(STUFFED_FROG_PROD);
 
-        const VALENTINE_BEAR_PROD_IN_CART = await CART_PAGE.product("Valentine Bear");
-        const VALENTINE_BEAR_PROD_CART_PRICE = await CART_PAGE.productDecimalPrice(VALENTINE_BEAR_PROD_IN_CART);
-        const VALENTINE_BEAR_PROD_CART_QUANTITY = await CART_PAGE.productQuantity(VALENTINE_BEAR_PROD_IN_CART);
-        const VALENTINE_BEAR_PROD_CART_SUBTOTAL = await CART_PAGE.productDecimalSubtotal(VALENTINE_BEAR_PROD_IN_CART);
+                await SHOP_PAGE.buy(STUFFED_FROG_PROD);
+                await SHOP_PAGE.buy(STUFFED_FROG_PROD);
 
-        await test.step("The subtotal for each product is correct", async () => {
-            expect.soft(
-                    parseFloat(STUFFED_FROG_PROD_CART_SUBTOTAL),
-                    "The Stuffed Frog's subtotal equals the product of its price and quantity"
-                ).toEqual(
-                    parseFloat(STUFFED_FROG_PROD_CART_PRICE) *
-                        parseFloat(STUFFED_FROG_PROD_CART_QUANTITY)
+                const FLUFFY_BUNNY_PROD = await SHOP_PAGE.product("Fluffy Bunny");
+                const FLUFFY_BUNNY_PROD_PRICE = await SHOP_PAGE.productDecimalPrice(FLUFFY_BUNNY_PROD);
+
+                await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
+                await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
+                await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
+                await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
+                await SHOP_PAGE.buy(FLUFFY_BUNNY_PROD);
+
+                const VALENTINE_BEAR_PROD = await SHOP_PAGE.product("Valentine Bear");
+                const VALENTINE_BEAR_PROD_PRICE = await SHOP_PAGE.productDecimalPrice(VALENTINE_BEAR_PROD);
+
+                await SHOP_PAGE.buy(VALENTINE_BEAR_PROD);
+                await SHOP_PAGE.buy(VALENTINE_BEAR_PROD);
+                await SHOP_PAGE.buy(VALENTINE_BEAR_PROD);
+
+                await test.step("Navigate to 'Cart' page", async () => {
+                    navigateTo(page, CART_PAGE);
+                });
+
+                const STUFFED_FROG_PROD_IN_CART = await CART_PAGE.product("Stuffed Frog");
+                const STUFFED_FROG_PROD_CART_PRICE = await CART_PAGE.productDecimalPrice(STUFFED_FROG_PROD_IN_CART);
+                const STUFFED_FROG_PROD_CART_QUANTITY = await CART_PAGE.productQuantity(STUFFED_FROG_PROD_IN_CART);
+                const STUFFED_FROG_PROD_CART_SUBTOTAL = await CART_PAGE.productDecimalSubtotal(STUFFED_FROG_PROD_IN_CART);
+
+                const FLUFFY_BUNNY_PROD_IN_CART = await CART_PAGE.product("Fluffy Bunny");
+                const FLUFFY_BUNNY_PROD_CART_PRICE = await CART_PAGE.productDecimalPrice(FLUFFY_BUNNY_PROD_IN_CART);
+                const FLUFFY_BUNNY_PROD_CART_QUANTITY = await CART_PAGE.productQuantity(FLUFFY_BUNNY_PROD_IN_CART);
+                const FLUFFY_BUNNY_PROD_CART_SUBTOTAL = await CART_PAGE.productDecimalSubtotal(FLUFFY_BUNNY_PROD_IN_CART);
+
+                const VALENTINE_BEAR_PROD_IN_CART = await CART_PAGE.product("Valentine Bear");
+                const VALENTINE_BEAR_PROD_CART_PRICE = await CART_PAGE.productDecimalPrice(VALENTINE_BEAR_PROD_IN_CART);
+                const VALENTINE_BEAR_PROD_CART_QUANTITY = await CART_PAGE.productQuantity(VALENTINE_BEAR_PROD_IN_CART);
+                const VALENTINE_BEAR_PROD_CART_SUBTOTAL = await CART_PAGE.productDecimalSubtotal(VALENTINE_BEAR_PROD_IN_CART);
+
+                await test.step("The subtotal for each product is correct", async () => {
+                    expect.soft(
+                            parseFloat(STUFFED_FROG_PROD_CART_SUBTOTAL),
+                            "The Stuffed Frog's subtotal equals the product of its price and quantity"
+                        ).toEqual(
+                            parseFloat(STUFFED_FROG_PROD_CART_PRICE) *
+                                parseFloat(STUFFED_FROG_PROD_CART_QUANTITY)
+                        );
+
+                    expect(
+                        parseFloat(FLUFFY_BUNNY_PROD_CART_SUBTOTAL),
+                        "The Fluffy Bunny's subtotal equals the product of its price and quantity"
+                    ).toEqual(
+                        parseFloat(FLUFFY_BUNNY_PROD_CART_PRICE) *
+                            parseFloat(FLUFFY_BUNNY_PROD_CART_QUANTITY)
+                    );
+
+                    expect(
+                        parseFloat(VALENTINE_BEAR_PROD_CART_SUBTOTAL),
+                        "The Valentine Bear's subtotal equals the product of its price and quantity"
+                    ).toEqual(
+                        parseFloat(VALENTINE_BEAR_PROD_CART_PRICE) *
+                            parseFloat(VALENTINE_BEAR_PROD_CART_QUANTITY)
+                    );
+                });
+
+                await test.step("The price for each product is correct", async () => {
+                    expect(
+                        STUFFED_FROG_PROD_CART_PRICE,
+                        "The price of the Stuffed Frog in the cart equals that on the 'Shop' page"
+                    ).toEqual(STUFFED_FROG_PROD_PRICE);
+
+                    expect(
+                        FLUFFY_BUNNY_PROD_CART_PRICE,
+                        "The price of the Fluffy Bunny in the cart equals that on the 'Shop' page"
+                    ).toEqual(FLUFFY_BUNNY_PROD_PRICE);
+
+                    expect(
+                        VALENTINE_BEAR_PROD_CART_PRICE,
+                        "The price of the Valentine Bear in the cart equals that on the 'Shop' page"
+                    ).toEqual(VALENTINE_BEAR_PROD_PRICE);
+                });
+
+                await expect(
+                    await CART_PAGE.totalPrice(),
+                    "The total equals the sum of all subtotals"
+                ).toContainText(
+                    (
+                        parseFloat(STUFFED_FROG_PROD_CART_SUBTOTAL) +
+                        parseFloat(FLUFFY_BUNNY_PROD_CART_SUBTOTAL) +
+                        parseFloat(VALENTINE_BEAR_PROD_CART_SUBTOTAL)
+                    ).toString()
                 );
-
-            expect(
-                parseFloat(FLUFFY_BUNNY_PROD_CART_SUBTOTAL),
-                "The Fluffy Bunny's subtotal equals the product of its price and quantity"
-            ).toEqual(
-                parseFloat(FLUFFY_BUNNY_PROD_CART_PRICE) *
-                    parseFloat(FLUFFY_BUNNY_PROD_CART_QUANTITY)
-            );
-
-            expect(
-                parseFloat(VALENTINE_BEAR_PROD_CART_SUBTOTAL),
-                "The Valentine Bear's subtotal equals the product of its price and quantity"
-            ).toEqual(
-                parseFloat(VALENTINE_BEAR_PROD_CART_PRICE) *
-                    parseFloat(VALENTINE_BEAR_PROD_CART_QUANTITY)
-            );
-        });
-
-        await test.step("The price for each product is correct", async () => {
-            expect(
-                STUFFED_FROG_PROD_CART_PRICE,
-                "The price of the Stuffed Frog in the cart equals that on the 'Shop' page"
-            ).toEqual(STUFFED_FROG_PROD_PRICE);
-
-            expect(
-                FLUFFY_BUNNY_PROD_CART_PRICE,
-                "The price of the Fluffy Bunny in the cart equals that on the 'Shop' page"
-            ).toEqual(FLUFFY_BUNNY_PROD_PRICE);
-
-            expect(
-                VALENTINE_BEAR_PROD_CART_PRICE,
-                "The price of the Valentine Bear in the cart equals that on the 'Shop' page"
-            ).toEqual(VALENTINE_BEAR_PROD_PRICE);
-        });
-
-        await expect(
-            await CART_PAGE.totalPrice(),
-            "The total equals the sum of all subtotals"
-        ).toContainText(
-            (
-                parseFloat(STUFFED_FROG_PROD_CART_SUBTOTAL) +
-                parseFloat(FLUFFY_BUNNY_PROD_CART_SUBTOTAL) +
-                parseFloat(VALENTINE_BEAR_PROD_CART_SUBTOTAL)
-            ).toString()
+            }
         );
-    });
-});
+    }
+);
